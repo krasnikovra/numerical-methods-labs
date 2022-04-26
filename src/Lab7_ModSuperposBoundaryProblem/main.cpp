306a5609fe65dad3e4b0c5bfd73c7b6667ec2076
krasnikovra <roma_krasnikov02@mail.ru>
2022-04-26 19:44:30 +0300

    upd

--- src/Lab7_ModSuperposBoundaryProblem/main.cpp
+++ src/Lab7_ModSuperposBoundaryProblem/main.cpp
@@ -71,8 +71,8 @@ int main() {
     const MathFunc q = [](const double x) -> double { return 1.0 + Pow(sin(x), 2); };
     const MathFunc r = [](const double x) -> double { return Pow(cos(x), 2); };
     const MathFunc f = [](const double x) -> double { return 3.0 * exp(x); };
-    const double a = 1.0;
-    const double b = 0.0;
+    const double a = 0.0;
+    const double b = 1.0;
     const Vector alpha = { 1.0, 1.0 };
     const Vector beta = { 1.0, 0.0 };
     const double A = alpha[0] * solEx(a) + alpha[1] * solExDer(a);
